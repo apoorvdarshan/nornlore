bb7cf35893f26005f7cdfc681eda68c75e39b0dd
Apoorv Darshan <ad13dtu@gmail.com>
2026-03-23 04:28:28 +0530

    Remove base path and site config for local development

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://apoorvdarshan.github.io',
-  base: '/nornlore/',
   output: 'static',
 });
